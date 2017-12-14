0209d16e5e422a67676b9cd3bc0d83de30fe509c
William <w.asiata@gmail.com>
2017-12-14 18:07:54 +1300

    Finally works - check out what the alg presently thinks is te reo LOLOL!

--- hansard_scraper.py
+++ hansard_scraper.py
@@ -1,7 +1,7 @@
 # import libraries
 import csv
 import re
-from collections import deque
+from pathlib import Path
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
 from datetime import datetime
@@ -9,12 +9,6 @@ from taumahi import tatau_tupu
 
 hansard_url = 'https://www.parliament.nz'
 hansard_meta_url = '{}{}'.format(hansard_url, '/en/document/')
-# doc_url = '{}{}'.format(hansard_doc_url, '123456789')
-# meta_url = '{}{}'.format(doc_url, '/metadata')
-# print(hansard_url)
-# print(hansard_doc_url)
-# print(doc_url)
-# print(meta_url)
 
 
 class Transcript:
@@ -53,6 +47,8 @@ class HansardTuhingaScraper:
         self.soup = bs(urlopen('{}{}'.format(
             hansard_url, self.doc_url)), 'html.parser')
 
+        self.retreived = datetime.now()
+
         doc_id = self.doc_url.split('/')[6]
 
         if re.match(r'\d', doc_id):
@@ -68,31 +64,31 @@ class HansardTuhingaScraper:
 
     def horoi_transcript_factory(self):
 
-        print(self.doc_url)
-
         meta_entries = self.metasoup.find_all('td')
+
+        doc_url = '{}{}'.format(hansard_url, self.doc_url)
         wā = meta_entries[1].get_text()
         title = meta_entries[0].get_text()
 
         transcripts = []
-
         teReo_size = 0
         total_size = 0
         awaiting_teReo = None
-
         section_count = 0
+
+        print('\n{}\n'.format(doc_url))
 
         for section in self.kōrero_hupo:
 
             section_count += 1
-            print('section:', section_count)
+            # print('section:', section_count)
             paragraph_count = 0
 
             ingoa_kaikōrero = ''
 
             for paragraph in section.find_all('p'):
 
-                print('paragraph: ', paragraph_count)
+                # print('paragraph: ', paragraph_count)
 
                 strong_tags = paragraph.find_all('strong')
 
@@ -114,12 +110,13 @@ class HansardTuhingaScraper:
 
                 if re.search(r'[a-zA-Z]', kōrero_waenga):
                     paragraph_count += 1
-                    num_Māori, num_non_Māori = tatau_tupu(paragraph)
+                    num_Māori, size = tatau_tupu(kōrero_waenga)
 
                     teReo_size += num_Māori
-                    total_size += num_non_Māori
+                    total_size += size
 
-                    heMāori = num_Māori / num_non_Māori
+                    if size:
+                        heMāori = (num_Māori / size) * 100
 
                     save_corpus = heMāori > 50
                     if not save_corpus:
@@ -129,10 +126,10 @@ class HansardTuhingaScraper:
                             awaiting_teReo = True
 
                     if save_corpus:
-                        print('{}: {}\nsection {}, paragraph {}, {}%\nname:{}\n{}\n'.format(
+                        print('{}: {}\nsection {}, paragraph {}, Maori = {}%\nname:{}\n{}\n'.format(
                             wā, title, section_count,
                             paragraph_count, heMāori, ingoa_kaikōrero, kōrero_waenga))
-                        transcripts.append(Transcript(doc_url='{}{}'.format(hansard_url, self.doc_url),
+                        transcripts.append(Transcript(doc_url=doc_url,
                                                       wā=wā,
                                                       title=title,
                                                       section=section_count,
@@ -140,46 +137,53 @@ class HansardTuhingaScraper:
                                                       ingoa_kaikōrero=ingoa_kaikōrero,
                                                       heMāori=heMāori,
                                                       kōrero_waenga=kōrero_waenga))
-        return transcripts, teReo_size, total_size, awaiting_teReo
+        doc_record = [self.retreived, self.doc_url, wā, title, teReo_size,
+                      total_size, teReo_size / total_size, awaiting_teReo]
+        return transcripts, doc_record
 
 
 def scrape_Hansard_URLs():
-    list_url = '{}{}'.format(hansard_url, '/en/pb/hansard-debates/rhr/')
-    rhr_soup = bs(urlopen(list_url), 'html.parser')
-    filename = 'urlindex.csv’
+    filename = 'urlindex.csv'
 
     has_header = False
     doc_url_list = []
 
-    with open(filename, 'r') as url_file:
-        has_header = csv.Sniffer().has_header(url_file)
-        if has_header:
+    if Path(filename).exists():
+        with open(filename, 'r') as url_file:
             for row in csv.DictReader(url_file):
                 doc_url_list.append(row['url'])
-
-    if not has_header:
+    else:
         with open(filename, 'w') as url_file:
-            head_writer = csv.writer(url_file)
-            head_writer.writerow(['Date retreived', 'url'])
+            csv.writer(url_file).writerow(['Date retreived', 'url'])
 
-    last_url = doc_url_list[-1]
+    last_url = ''
+    if doc_url_list:
+        last_url = doc_url_list[-1]
     new_list = get_new_urls(last_url)
 
     with open(filename, 'a') as url_file:
+        url_writer = csv.writer(url_file)
         for url in reversed(new_list):
             doc_url_list.append(url[1])
             url_writer.writerow(url)
+
+    print('\nCollected all URLs\n')
 
     return doc_url_list
 
 
 def get_new_urls(last_url):
+    rhr_soup = bs(urlopen('{}{}'.format(
+        hansard_url, '/en/pb/hansard-debates/rhr/')), 'html.parser')
+
     new_list = []
     while True:
+        print('\nChecking for new kōrerorero Hansard\n')
+
         retreivedtime = datetime.now()
         for h2 in rhr_soup.select('ul.hansard__list h2'):
             new_url = h2.a['href']
-            if new_url is last_url:
+            if new_url == last_url:
                 return new_list
             else:
                 print(new_url)
@@ -202,21 +206,18 @@ def aggregate_hansard_corpus(doc_urls):
     corpusfilename = 'hansardcorpus.csv'
     recordfilename = 'hansardrecord.csv'
 
-    record_has_header = False
     record_list = []
     waiting_for_reo_list = []
 
-    with open(recordfilename, 'r') as record_file:
-        record_has_header = csv.Sniffer().has_header(record_file)
-        if record_has_header:
+    if Path(recordfilename).exists():
+        with open(recordfilename, 'r') as record_file:
             rowcount = 0
             for row in csv.DictReader(record_file):
                 record_list.append(row)
-                rowcount + = 1
                 if row['awaiting authorised reo'] is True:
                     waiting_for_reo_list.append(rowcount)
-
-    if not record_has_header:
+                rowcount += 1
+    else:
         with open(recordfilename, 'w') as record_file:
             head_writer = csv.writer(record_file)
             head_writer.writerow([
@@ -230,16 +231,14 @@ def aggregate_hansard_corpus(doc_urls):
                 'awaiting authorised reo'
             ])
 
-    corpus_has_header = False
-    record_list = []
+        # corpus_list = []
 
-    with open(corpusfilename, 'r') as corpus:
-        corpus_has_header = csv.Sniffer().has_header(corpus)
-        if corpus_has_header:
-            for row in csv.DictReader(record_file):
-                record_list.append(row)
-
-    if not corpus_has_header:
+    if not Path(corpusfilename).exists():
+        #     with open(corpusfilename, 'r') as corpus:
+        #         if corpus_has_header:
+        #             for row in csv.DictReader(corpus):
+        #                 record_list.append(row)
+        # else:
         with open(corpusfilename, 'w') as corpus:
             head_writer = csv.writer(corpus)
             head_writer.writerow([
@@ -253,8 +252,13 @@ def aggregate_hansard_corpus(doc_urls):
                 'kōrero waenga'
             ])
 
-    last_record_url = record_list[-1]['Hansard document url']
-    remaining_urls = doc_urls[doc_urls.index(last_record_url) + 1:]get_more_urls(last_record_url, doc_urls)
+    remaining_urls = []
+
+    if record_list:
+        last_record_url = record_list[-1]['Hansard document url']
+        remaining_urls = doc_urls[doc_urls.index(last_record_url) + 1:]
+    else:
+        remaining_urls = doc_urls
 
     with open(recordfilename, 'a') as record:
         with open(corpusfilename, 'a') as kiwaho:
@@ -268,14 +272,13 @@ def aggregate_hansard_corpus(doc_urls):
 
 
 def corpus_writer(doc_url, record_csv, hansard_csv):
-    transcripts, teReo_size, total_size, awaiting_teReo = HansardTuhingaScraper(
+    transcripts, doc_record = HansardTuhingaScraper(
         doc_url).horoi_transcript_factory()
 
-    record_csv.writerow([datetime.now()] + transcripts[0].listify[0:3] + [
-                        teReo_size, total_size, teReo_size / total_size, awaiting_teReo])
-
-    for transcript in transcripts:
-        hansard_csv.writerow(transcript.listify())
+    record_csv.writerow(doc_record)
+    if transcripts:
+        for transcript in transcripts:
+            hansard_csv.writerow(transcript.listify())
 
 
 def main():
