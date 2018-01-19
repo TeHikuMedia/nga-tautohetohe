f63924bc04e22b2d0487bc8f09a211561c705f2a
William <w.asiata@gmail.com>
2018-01-19 14:16:00 +1300

    Optimised downloading algorithm

--- historicalhansard_WEBscraper.py
+++ historicalhansard_WEBscraper.py
@@ -4,7 +4,7 @@ import re
 import time
 from os import mkdir
 from pathlib import Path
-from multiprocessing.dummy import Pool as ThreadPool
+from multiprocessing.dummy import Process, Pool as ThreadPool, Lock
 from urllib.request import urlopen
 from bs4 import BeautifulSoup as bs
 from datetime import datetime
@@ -18,7 +18,8 @@ volumes_dir = 'volumes'
 class Volume:
     """docstring for Volume."""
 
-    def __init__(self, name, url, period, session):
+    def __init__(self, name, url, period, session, row=None):
+        self.row = row
         self.name = name
         self.url = url
         self.period = period
@@ -32,22 +33,26 @@ def get_volume_meta():
     print('Getting volume URLs:\n')
 
     volumes = []
-
+    count = 0
     if Path(filename).exists():
         with open(filename, 'r') as url_file:
             for row in csv.DictReader(url_file):
-                print('Volume', row['name'], row['url'])
-                volumes.append(
-                    Volume(row['name'], row['url'], row['period'], row['session']))
+                count += 1
+                if not row['downloaded']:
+                    print('Volume', row['name'], row['url'])
+                    volumes.append(
+                        Volume(row['name'], row['url'], row['period'], row['session'], count))
 
-    if len(volumes) < 487:
+    if count < 480:
         with open(filename, 'w') as url_file:
             writer = csv.writer(url_file)
             writer.writerow(
-                ['retreived', 'url', 'name', 'period', 'session'])
+                ['retreived', 'url', 'name', 'period', 'session', 'downloaded'])
             volumes = scrape_volume_urls(writer)
 
-    print('\nCollected Hathi volume URLs after {}\n'.format(get_rate()))
+    print('\nCollected Hathi volume URLs after {}'.format(get_rate(start_time)))
+    print('{}/{} ({}%) volumes downloaded\n'.format(len(volumes),
+                                                    count, round(100 * (1 - len(volumes) / count), 2)))
 
     return volumes
 
@@ -59,7 +64,7 @@ def scrape_volume_urls(writer):
         '.wikitable')[0]('tr')[1:]
 
     breaker = None
-
+    count = 0
     for tr in volume_directory:
         tds = tr('td')
         switch = None
@@ -80,9 +85,10 @@ def scrape_volume_urls(writer):
                     period = td.string.strip()
                     switch = True
         print('Volume {}; {}\n{}\n{}\n'.format(name, period, session, href))
+        count += 1
         writer.writerow([retreived, href, name, period, session])
         volumes.append(Volume(name=name, url=href,
-                              period=period, session=session))
+                              period=period, session=session, row=count))
         if breaker:
             break
     return volumes
@@ -93,246 +99,143 @@ def download_volumes():
     if not Path(volumes_dir).exists():
         mkdir(volumes_dir)
 
-    pool = ThreadPool(6)
-    results = pool.map(download_volume, volumes)
+    t = time.time()
+    with ThreadPool(num_threads) as pool:
+        results = pool.map(download_volume, volumes)
+
+    print("\n--- {} volumes downloaded in {} ---".format(len(volumes), get_rate(t)))
+
+
+write_lock = Lock()
 
 
 def download_volume(volume):
-    filepath = '{}/{}.csv'.format(volumes_dir, volume.name)
-    rowcount = 0
+    name = volume.name
+    url = ''
+    pagecount = 0
+    fieldnames = []
 
+    print('Downloading volume {}'.format(name))
+
+    filepath = '{}/{}.csv'.format(volumes_dir, name)
     if Path(filepath).exists():
         with open(filepath, 'r') as txt_file:
-            for row in csv.DictReader(txt_file):
-                rowcount += 1
-
-    if rowcount < 100:
+            reader = csv.DictReader(txt_file)
+            fieldnames = reader.fieldnames
+            for row in reader:
+                url = row['url']
+                pagecount = int(row['page'])
+    else:
         with open(filepath, 'w') as txt_file:
+            fieldnames = ['retreived', 'url', 'page', 'text']
             writer = csv.writer(txt_file)
-            writer.writerow(['retreived', 'url', 'name', 'page', 'text'])
-            url = volume.url
-            pagecount = 0
-            while True:
-                pagecount += 1
-                url = '{}{}'.format(hathi_domain, url)
+            writer.writerow(fieldnames)
 
-                # reset attempt counter
-                tries = 1
-                soup = None
-                while True:
-                    try:
-                        # parse the page and break if successful
-                        soup = bs(urlopen(url), 'html.parser')
-                        tries += 1
-                        break
-                    except Exception as e:
-                        # sleep briefly if there's an error
-                        tries += 1
-                        print('Attempt {} retrieving: {}\n'.format(tries, url))
-                        time.sleep(4)
+    condition = True
+    if url:
+        condition, url, _ = download_page(
+            url.replace(hathi_domain, '', 1), name, pagecount)
+    else:
+        url = volume.url
 
-                page_soup = soup.find(id='mdpPage')
-                text = page_soup.find(class_='Text')
-                if text:
-                    writer.writerow(
-                        [datetime.now(), url, volume.name, pagecount, text.string])
-                    print('Volume {}, page {}'.format(
-                        volume.name, pagecount))
+    with open(filepath, 'a') as txt_file:
+        writer = csv.DictWriter(txt_file, fieldnames=fieldnames)
+        while condition:
+            pagecount += 1
+            condition, url, row = download_page(url, name, pagecount)
+            if row:
+                writer.writerow(row)
+                print('Volume {}, page {}'.format(name, row['page']))
 
-                url = page_soup('a')[1]['href']
-                if url == '#top':
-                    break
+    completion = 1
+    with write_lock:
+        filename = 'hathivolumeURLs.csv'
+        count, rows = 0, []
+        with open(filename, 'r') as url_file:
+            for row in csv.DictReader(url_file):
+                count += 1
+                if row['downloaded']:
+                    completion += 1
+                if volume.row == count:
+                    row['downloaded'] = True
+                rows.append(row)
+            completion = round(100 * completion / count, 2)
+
+        with open(filename, 'w') as url_file:
+            writer = csv.DictWriter(url_file, fieldnames=[
+                                    'retreived', 'url', 'name', 'period', 'session', 'downloaded'])
+            writer.writeheader()
+            for row in rows:
+                writer.writerow(row)
+
+    print('Volume {} complete! Downloading {}%% complete\n'.format(name, completion))
+    # p = Process()
 
 
-class HansardTuhingaScraper:
-    def __init__(self, doc_url):
-        ''' Set up our tuhituhi CorpusCollector with basic params '''
-        self.doc_url = doc_url
-        self.hanga_hupo()
+num_threads = 150  # Network can download 100+ pages between errors, so 100+ threads will maximise concurrent page download speed
+sleep_lock = Lock()
+responsive_lock = Lock()
+responsive = True
+tries = 0
+count_lock = Lock()
+total_pages_processed = 0
+interval_pages_processed = 0
 
-    def hanga_hupo(self):
-        # query the website and parse the returned html using beautiful soup
 
-        doc_id = self.doc_url.split('/')[6]
-        alternative_URL = '{}{}'.format(hansard_meta_url, doc_id)
-        get_stuff = ''
-        exception_flag = None
+def download_page(url, name, page):
+    url = '{}{}'.format(hathi_domain, url)
+    soup = None
+    global total_pages_processed, responsive, tries, interval_pages_processed
 
+    with responsive_lock:
+        while not responsive:
+            time.sleep(0.5)
+    while True:
         try:
-            get_stuff = urlopen('{}{}'.format(
-                hansard_url, self.doc_url))
+            # parse the page and break if successful
+            soup = bs(urlopen(url), 'html.parser')
+            responsive = True
+            break
         except Exception as e:
-            print(e, '\nTrying alternative URL...')
-            try:
-                get_stuff = urlopen(alternative_URL)
-                exception_flag = True
-                print('\nSuccess!\n')
-            except Exception as e:
-                raise Exception(e, '\nCould not find data')
+            responsive = False
+            with sleep_lock:
+                if not responsive:
+                    print('Downloaded {} since last error -'.format(
+                        interval_pages_processed), e)
+                    time.sleep(1)
+                    if not responsive:
+                        time.sleep(4)
+                        tries += 1
+                        print(
+                            'Attempt {} - retrieving: volume {}, page {}'.format(tries, name, page))
 
-        self.soup = bs(get_stuff, 'html.parser')
+    with count_lock:
+        if tries > 0:
+            # reset attempt counter
+            interval_pages_processed = 0
+            tries = 0
+            print('Downloaded {} pages in {} at {} p/s'.format(
+                total_pages_processed, get_rate(start_time), round(total_pages_processed / (time.time() - start_time), 2)))
+        total_pages_processed += 1
+        interval_pages_processed += 1
 
-        self.retreived = datetime.now()
+    row = {}
+    page_soup = soup.find(id='mdpPage')
+    text = page_soup.find(class_='Text')
+    if text:
+        row = {'retreived': datetime.now(), 'url': url, 'page': page,
+               'text': text.string}
 
-        if exception_flag:
-            self.kōrero_hupo = self.soup.find('div', attrs={'class': 'section'}).select(
-                'div.section > div.section')
-        elif re.match(r'\d', doc_id):
-            self.kōrero_hupo = self.soup.select('div.Hansard > div')
-        else:
-            self.kōrero_hupo = self.soup.find_all(
-                'div', attrs={'class': 'section'})
-
-        # Make soup from hansard metadata
-        meta_url = '{}{}'.format(alternative_URL, '/metadata')
-        self.metasoup = bs(urlopen(meta_url), 'html.parser').table
-
-    def horoi_transcript_factory(self):
-
-        meta_entries = self.metasoup.find_all('td')
-
-        doc_url = '{}{}'.format(hansard_url, self.doc_url)
-        wā = meta_entries[1].get_text()
-        title = meta_entries[0].get_text()
-
-        transcripts = []
-        totals = [0, 0, 0]
-        awaiting_teReo = None
-        section_count = 0
-
-        print('\n{}\n'.format(doc_url))
-
-        for section in self.kōrero_hupo:
-
-            section_count += 1
-            paragraph_count = 0
-            ingoa_kaikōrero = ''
-
-            p_list = section.find_all('p')
-            print('Paragraphs =', len(p_list))
-
-            for paragraph in p_list:
-
-                strong_tags = paragraph.find_all('strong')
-
-                flag = False
-
-                for strong in strong_tags:
-                    string = strong.extract().string
-                    if not flag and string and re.search(r'[a-zA-Z]{5,}', string):
-                        ingoa_kaikōrero = string.strip()
-                        flag = True
-
-                kōrero = paragraph.get_text(strip=True)
-                check = None
-                if re.match(r'\[.*\]', kōrero):
-                    if re.match(r'\[Authorised Te Reo text', kōrero):
-                        check = True
-                        awaiting_teReo = True
-                    else:
-                        continue
-
-                if flag:
-                    p = kōrero.split(':', 1)[-1].strip()
-                    if p:
-                        kōrero = p
-
-                if re.search(r'[a-zA-Z]', kōrero):
-                    paragraph_count += 1
-
-                    save_corpus, numbers = kupu_ratios(kōrero)
-
-                    for i in range(len(totals)):
-                        totals[i] = totals[i] + numbers[i]
-
-                    if save_corpus or check:
-                        print('{}: {}\nsection {}, paragraph {}, Maori = {}%\nname:{}\n{}\n'.format(
-                            wā, title, section_count,
-                            paragraph_count, numbers[3], ingoa_kaikōrero, kōrero))
-                        transcripts.append([doc_url, wā, title, section_count, paragraph_count,
-                                            ingoa_kaikōrero] + numbers + [clean_whitespace(kōrero)])
-        print('Time:', self.retreived)
-        doc_record = [self.retreived, self.doc_url, wā, title] + totals + \
-            [get_percentage(totals[0], totals[1], totals[2]), awaiting_teReo]
-        return transcripts, doc_record
-
-
-def aggregate_hansard_corpus(doc_urls):
-    transcripts = []
-
-    corpusfilename = 'historicalhansardcorpus.csv'
-    recordfilename = 'historicalhansardindex.csv'
-
-    record_list = []
-    waiting_for_reo_list = []
-
-    if Path(recordfilename).exists():
-        with open(recordfilename, 'r') as record_file:
-            rowcount = 0
-            for row in csv.DictReader(record_file):
-                record_list.append(row)
-                if row['awaiting authorised reo'] is True:
-                    waiting_for_reo_list.append(rowcount)
-                rowcount += 1
+    url = ''
+    anchors = page_soup('a')
+    if page == 1:
+        for a in anchors:
+            if a.get_text().strip() == 'Next Page':
+                url = a['href']
     else:
-        with open(recordfilename, 'w') as record_file:
-            head_writer = csv.writer(record_file)
-            head_writer.writerow([
-                'Date retreived',
-                'Hansard document url',
-                'Wā',
-                'Title',
-                'Te Reo length',
-                'Ambiguous length',
-                'Other length',
-                'Is Māori (%)',
-                'Awaiting authorised reo'
-            ])
+        url = anchors[1]['href']
 
-    if not Path(corpusfilename).exists():
-        with open(corpusfilename, 'w') as corpus:
-            head_writer = csv.writer(corpus)
-            head_writer.writerow([
-                'Hansard document url',
-                'Wā',
-                'Title',
-                'Section number',
-                'Utterance number',
-                'Ingoa kaikōrero',
-                'Te Reo length',
-                'Ambiguous length',
-                'Other length',
-                'Is Māori (%)',
-                'Kōrero'
-            ])
-
-    remaining_urls = []
-
-    if record_list:
-        last_record_url = record_list[-1]['Hansard document url']
-        remaining_urls = doc_urls[doc_urls.index(last_record_url) + 1:]
-    else:
-        remaining_urls = doc_urls
-
-    with open(recordfilename, 'a') as record:
-        with open(corpusfilename, 'a') as kiwaho:
-            record_csv = csv.writer(record)
-            hansard_csv = csv.writer(kiwaho)
-
-            for doc_url in remaining_urls:
-                corpus_writer(doc_url, record_csv, hansard_csv)
-
-                print('---\n')
-
-
-def corpus_writer(doc_url, record_csv, hansard_csv):
-    transcripts, doc_record = HansardTuhingaScraper(
-        doc_url).horoi_transcript_factory()
-
-    record_csv.writerow(doc_record)
-    if transcripts:
-        for transcript in transcripts:
-            hansard_csv.writerow(transcript)
+    return url != '#top', url, row
 
 
 def main():
@@ -343,18 +246,16 @@ def main():
     except Exception as e:
         raise e
     finally:
-        print("\n--- Job took {} ---".format(get_rate()))
+        print("\n--- Job took {} ---".format(get_rate(start_time)))
         # print('Looped through {} strings while processing {}\n'.format(
         # most_loops, longest_day))
 
 
 start_time = time.time()
-longest_day = ''
-most_loops = 0
 
 
-def get_rate():
-    m, s = divmod(time.time() - start_time, 60)
+def get_rate(t):
+    m, s = divmod(time.time() - t, 60)
     s = int(s)
     h, m = divmod(m, 60)
     if m:
