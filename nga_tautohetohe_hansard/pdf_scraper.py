299391f1d278f42214319d15a1706186f80b20da
William <w.asiata@gmail.com>
2018-02-05 12:40:50 +1300

    standardised pdf scraper

--- nga_tautohetohe_hansard/pdf_scraper.py
+++ nga_tautohetohe_hansard/pdf_scraper.py
@@ -8,6 +8,11 @@ from taumahi import *
 from os import listdir
 from os.path import isfile, join
 
+dayindex_fieldnames = ['url', 'volume', 'date', 'reo', 'ambiguous', 'other',
+                       'percent', 'retrieved', 'format', 'incomplete']
+reo_fieldnames = ['url', 'volume', 'date', 'utterance', 'speaker', 'reo',
+                  'ambiguous', 'other', 'percent', 'text']
+
 
 class Speech:
     """docstring for Speech."""
@@ -30,44 +35,32 @@ page_break = re.compile('(\n{0,2}\d{1,2} [a-zA-Z]{3,9} \d{4}.*\n\n\f)')
 
 
 def process_txt_files(dirpath):
-    corpusfilename = 'hansardcorpusPDF.csv'
-    indexfilename = 'hansardindexPDF.csv'
+    corpusfilename = 'hansardreomāori.csv'
+    indexfilename = 'hansardrāindex.csv'
 
-    with open(indexfilename, 'w') as index:
-        index_csv = csv.writer(index)
-        index_csv.writerow([
-            'Hansard volume',
-            'Rā',
-            'Num paragraphs',
-            'Te Reo length',
-            'Ambiguous length',
-            'Other length',
-            'is Māori (%)',
-        ])
-        with open(corpusfilename, 'w') as kiwaho:
-            corpus_csv = csv.writer(kiwaho)
-            corpus_csv.writerow([
-                'Hansard volume',
-                'Rā',
-                'Ingoa kaikōrero',
-                'Speaker turn',
-                'Paragraph number',
-                'Te Reo length',
-                'Ambiguous length',
-                'Other length',
-                'Is Māori (%)',
-                'Kōrero'
-            ])
+    if not exists('hansardrāindex.csv'):
+        with open('hansardrāindex.csv', 'w') as f:
+            writer = csv.DictWriter(f, dayindex_fieldnames)
+            writer.writeheader()
+    if not exists('hansardreomāori.csv'):
+        with open('hansardreomāori.csv', 'w') as f:
+            writer = csv.DictWriter(f, reo_fieldnames)
+            writer.writeheader()
 
-            for f in get_file_list(dirpath):
-                print('\nProcessing {}:\n'.format(f))
-                with open('{}/{}'.format(dirpath, f), 'r') as hansard_txt:
-                    txt = sub_vowels(page_break.sub('\n', hansard_txt.read()))
-                    txt = re.sub(r'\[[^\]]*]', '', txt)
-                    tuhituhikifile(f, get_daily_debates(
-                        txt), index_csv, corpus_csv)
-                print('{} processed at {} after {}\n'.format(
-                    f, datetime.now(), get_rate()))
+    with open('hansardrāindex.csv', 'a') as i, open('hansardreomāori.csv', 'a') as c:
+        index_writer = csv.DictWriter(i, dayindex_fieldnames)
+        corpus_writer = csv.DictWriter(c, reo_fieldnames)
+        index_writer.writerows(i_rows)
+        corpus_writer.writerows(r_rows)
+        for f in get_file_list(dirpath):
+            print('\nProcessing {}:\n'.format(f))
+            with open('{}/{}'.format(dirpath, f), 'r') as hansard_txt:
+                txt = sub_vowels(page_break.sub('\n', hansard_txt.read()))
+                txt = re.sub(r'\[[^\]]*]', '', txt)
+                tuhituhikifile(f, get_daily_debates(
+                    txt), index_writer, corpus_writer)
+            print('{} processed at {} after {}\n'.format(
+                f, datetime.now(), get_rate()))
 
 
 def get_file_list(dirpath):
@@ -143,35 +136,30 @@ def get_speeches(txt):
     return speeches
 
 
-def tuhituhikifile(volume, debates, index_csv, corpus_csv):
-    volume_totals = [0, 0, 0]
+def tuhituhikifile(volume, debates, index_writer, corpus_writer):
     for date, speeches in reversed(debates):
-        totals = [0, 0, 0]
-
-        turn = 0
-        p_sum = 0
+        totals = {'reo': 0, 'ambiguous': 0, 'other': 0}
+        i_row = c_row = {'volume': volume[volume.index(
+            ' ') + 1:volume.index(':')], 'date': date}
+        c_row['utterance'] = 0
         for speech in speeches:
-            turn += 1
-            kaikōrero = speech.kaikōrero
-            p_count = 0
             for paragraph in speech.paragraphs:
-                p_count += 1
+                row['utterance'] += 1
                 if paragraph.condition:
-                    kōrero = clean_whitespace(paragraph.txt)
-                    print('{}: {}\nSpeaker {}: {}, paragraph {},\nMaori = {}%\n{}\n'.format(
-                        volume, date, turn, kaikōrero, p_count, paragraph.ratios[3], kōrero))
-                    corpus_csv.writerow(
-                        [volume, date, kaikōrero, turn, p_count] + paragraph.ratios + [kōrero])
-                for i in range(len(totals)):
-                    totals[i] += paragraph.ratios[i]
-            p_sum += p_count
-        index_csv.writerow([volume, date, p_sum] + totals +
-                           [get_percentage(totals[0], totals[1], totals[2])])
-        for i in range(len(totals)):
-            volume_totals[i] += totals[i]
-    print(
-        'Maori = {a}, Ambiguous = {b}, Non-Māori = {c}, Percentage = {d} %'.format(
-            a=volume_totals[0], b=volume_totals[1], c=volume_totals[2], d=get_percentage(volume_totals[0], volume_totals[1], volume_totals[2])))
+                    c_row.update({'text': clean_whitespace(
+                        paragraph.txt), 'speaker': speech.kaikōrero})
+                    c_row.update(paragraph.ratios)
+                    print(
+                        'Volume {volume}: {date}\n{speaker}: {utterance},\nMaori = {percent}%\n{text}\n'.format(**row))
+                    corpus_writer.writerow(c_row)
+
+                for k, v in paragraph.ratios.items():
+                    totals[k] += v
+        i_row.update({'percent': get_percentage(**totals), 'format': 'PDF'})
+        i_row.update(totals)
+        index_writer.writerow(i_row)
+        print(
+            'Maori = {reo}, Ambiguous = {ambiguous}, Non-Māori = {other}, Percentage = {percent} %'.format(**i_row))
 
 
 def main():
